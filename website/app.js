57439f56b75eb70eb7fcd111e1660fa09eb0d74c
Rich Ung <ricky196@yahoo.com>
2019-02-19 08:40:43 -0800

    Changing photos to audios to better match project

--- website/app.js
+++ website/app.js
@@ -80,8 +80,8 @@ function createAlbum(albumName) {
 }
 
 function viewAlbum(albumName) {
-  var albumPhotosKey = encodeURIComponent(albumName) + '//';
-  s3.listObjects({Prefix: albumPhotosKey}, function(err, data) {
+  var albumAudiosKey = encodeURIComponent(albumName) + '//';
+  s3.listObjects({Prefix: albumAudiosKey}, function(err, data) {
     if (err) {
       return alert('There was an error viewing your album: ' + err.message);
     }
@@ -89,36 +89,36 @@ function viewAlbum(albumName) {
     var href = this.request.httpRequest.endpoint.href;
     var bucketUrl = href + albumBucketName + '/';
 
-    var photos = data.Contents.map(function(photo) {
-      var photoKey = photo.Key;
-      var photoUrl = bucketUrl + encodeURIComponent(photoKey);
+    var audios = data.Contents.map(function(audio) {
+      var audioKey = audio.Key;
+      var audioUrl = bucketUrl + encodeURIComponent(audioKey);
       return getHtml([
         '<span>',
           '<div>',
-            '<span onclick="deletePhoto(\'' + albumName + "','" + photoKey + '\')">',
+            '<span onclick="deleteaudio(\'' + albumName + "','" + audioKey + '\')">',
               'X',
             '</span>',
             '<span>',
-              photoKey.replace(albumPhotosKey, ''),
+              audioKey.replace(albumAudiosKey, ''),
             '</span>',
           '</div>',
         '</span>',
       ]);
     });
-    var message = photos.length ?
-      '<p>Click on the X to delete the photo</p>' :
-      '<p>You do not have any photos in this album. Please add photos.</p>';
+    var message = audios.length ?
+      '<p>Click on the X to delete the audio</p>' :
+      '<p>You do not have any audios in this album. Please add audios.</p>';
     var htmlTemplate = [
       '<h2>',
         'Album: ' + albumName,
       '</h2>',
       message,
       '<div>',
-        getHtml(photos),
+        getHtml(audios),
       '</div>',
-      '<input id="photoupload" type="file" accept="audio/*">',
-      '<button id="addphoto" onclick="addPhoto(\'' + albumName +'\')">',
-        'Add Photo',
+      '<input id="audioupload" type="file" accept="audio/*">',
+      '<button id="addaudio" onclick="addaudio(\'' + albumName +'\')">',
+        'Add audio',
       '</button>',
       '<button onclick="listAlbums()">',
         'Back To Albums',
@@ -128,35 +128,35 @@ function viewAlbum(albumName) {
   });
 }
 
-function addPhoto(albumName) {
-  var files = document.getElementById('photoupload').files;
+function addaudio(albumName) {
+  var files = document.getElementById('audioupload').files;
   if (!files.length) {
     return alert('Please choose a file to upload first.');
   }
   var file = files[0];
   var fileName = file.name;
-  var albumPhotosKey = encodeURIComponent(albumName) + '//';
+  var albumAudiosKey = encodeURIComponent(albumName) + '//';
 
-  var photoKey = albumPhotosKey + fileName;
+  var audioKey = albumAudiosKey + fileName;
   s3.upload({
-    Key: photoKey,
+    Key: audioKey,
     Body: file,
     ACL: 'public-read'
   }, function(err, data) {
     if (err) {
-      return alert('There was an error uploading your photo: ', err.message);
+      return alert('There was an error uploading your audio: ', err.message);
     }
-    alert('Successfully uploaded photo.');
+    alert('Successfully uploaded audio.');
     viewAlbum(albumName);
   });
 }
 
-function deletePhoto(albumName, photoKey) {
-  s3.deleteObject({Key: photoKey}, function(err, data) {
+function deleteaudio(albumName, audioKey) {
+  s3.deleteObject({Key: audioKey}, function(err, data) {
     if (err) {
-      return alert('There was an error deleting your photo: ', err.message);
+      return alert('There was an error deleting your audio: ', err.message);
     }
-    alert('Successfully deleted photo.');
+    alert('Successfully deleted audio.');
     viewAlbum(albumName);
   });
 }
