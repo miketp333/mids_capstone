6092a80b9b09b912df1b640a7481aa1d85093193
Rich Ung <ricky196@yahoo.com>
2019-04-09 10:09:19 -0700

    Updating lambda function to use backend URL

--- lambda/capstone-create-translation/lambda_function.py
+++ lambda/capstone-create-translation/lambda_function.py
@@ -3,6 +3,7 @@ import boto3
 import random
 from boto3.dynamodb.conditions import Key, Attr
 from botocore.exceptions import ClientError
+import requests
 
 # Helper class to convert a DynamoDB item to JSON.
 class DecimalEncoder(json.JSONEncoder):
@@ -34,15 +35,24 @@ def lambda_handler(event, context):
     
     for record in event['Records']:
         s3_key = record['s3']['object']['key']
+        album_name = s3_key.split('/')[0]
         print('Found s3_key: '+s3_key)
-        response = translations_table.put_item(
-           Item={
-                's3_file': s3_key,
-                'translation': translations_list[random.randint(0,10)]
-            }
-        )
+        try:
+            response = translations_table.put_item(
+               Item={
+                    's3_file': s3_key,
+                    'album_name': album_name,
+                    'pred_translation': translations_list[random.randint(0,10)]
+                },
+                ConditionExpression='attribute_not_exists(s3_file)'
+            )
+        except:
+            pass
+        URL = "http://w210backend-nlb-d9bcece2494a9651.elb.us-east-1.amazonaws.com/translate/"+s3_key
+        r = requests.get(url = URL)
     
     return {
         'statusCode': 200,
-        'body': json.dumps(response, indent=4, cls=DecimalEncoder)
+        # 'body': json.dumps(response, indent=4, cls=DecimalEncoder)
+        'body': json.dumps(r.json())
     }
