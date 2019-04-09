83c05922aa4ed4f1f14db7125c6827f8066da3fb
Rich Ung <ricky196@yahoo.com>
2019-04-09 15:26:33 -0700

    Automatically setting translation to Unknown Phrase if translation fails

--- translations_backend/service/translations_backend.py
+++ translations_backend/service/translations_backend.py
@@ -35,46 +35,48 @@ class DecimalEncoder(json.JSONEncoder):
 
 @app.route('/')
 def index():
-    return 'Translation Index Page'
+    return 'Translation Index Page, Version 1.1'
 
 @app.route('/translate/<path:s3_key>')
 def translate(s3_key):
-    album_name = s3_key.split('/')[0]
+    try:
+        album_name = s3_key.split('/')[0]
 
-    s3 = session.resource("s3")
-    dynamodb = session.resource("dynamodb",
-        region_name='us-east-1')
-    translations_table = dynamodb.Table('audio_translations')
+        s3 = session.resource("s3")
+        dynamodb = session.resource("dynamodb",
+            region_name='us-east-1')
+        translations_table = dynamodb.Table('audio_translations')
 
-    training_s3_files = translations_table.scan(
-        FilterExpression=Key('s3_file').begins_with(album_name+'/') & Attr('is_training').eq(True)
-    )
+        training_s3_files = translations_table.scan(
+            FilterExpression=Key('s3_file').begins_with(album_name+'/') & Attr('is_training').eq(True)
+        )
 
-    training_filenames = []
-    y_train = []
+        training_filenames = []
+        y_train = []
 
-    for i in training_s3_files['Items']:
-        # training_filenames.append(i['s3_file'])
-        try:
-            filename = './'+i['s3_file'].split('/')[2]
-            s3.Bucket('capstone-dysarthrai-website-guest-upload').download_file(i['s3_file'], filename)
-            training_filenames.append(filename)
-            y_train.append(i['actual_translation'])
-        except:
-            pass
+        for i in training_s3_files['Items']:
+            try:
+                filename = './'+i['s3_file'].split('/')[2]
+                s3.Bucket('capstone-dysarthrai-website-guest-upload').download_file(i['s3_file'], filename)
+                training_filenames.append(filename)
+                y_train.append(i['actual_translation'])
+            except:
+                pass
 
-    assert len(training_filenames) == len(y_train)
+        assert len(training_filenames) == len(y_train)
 
-    test_filename = './'+s3_key.split('/')[2]
-    s3.Bucket('capstone-dysarthrai-website-guest-upload').download_file(s3_key, test_filename)
-    test_s3_info = translations_table.query(KeyConditionExpression=Key('s3_file').eq(s3_key))
+        test_filename = './'+s3_key.split('/')[2]
+        s3.Bucket('capstone-dysarthrai-website-guest-upload').download_file(s3_key, test_filename)
+        test_s3_info = translations_table.query(KeyConditionExpression=Key('s3_file').eq(s3_key))
 
-    x_train, x_test, train_len, test_len = translation_model.generate_mfcc_lists(training_filenames, [test_filename])
+        x_train, x_test, train_len, test_len = translation_model.generate_mfcc_lists(training_filenames, [test_filename])
 
-    assert len(x_train) == len(y_train)
+        assert len(x_train) == len(y_train)
 
-    master_dist = translation_model.calc_dtw(x_train, x_test, train_len, test_len, radius=1)
-    prediction = translation_model.prediction(master_dist, y_train, test_len)
+        master_dist = translation_model.calc_dtw(x_train, x_test, train_len, test_len, radius=1)
+        prediction = translation_model.prediction(master_dist, y_train, test_len)
+    except:
+        prediction = ['Unknown Phrase']
 
     response = translations_table.update_item(
         Key={
